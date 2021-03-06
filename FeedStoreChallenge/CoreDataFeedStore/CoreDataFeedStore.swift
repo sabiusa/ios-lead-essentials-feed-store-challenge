4a24c1d2f1c3a9c609c3f4988d5091616dd40cd0
sabiusa <sabakhuts@gmail.com>
2021-03-06 16:01:45 +0400

    `CoreDataFeedStore.retrieve()` delivers found values on non-empty cache

--- FeedStoreChallenge/CoreDataFeedStore/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore/CoreDataFeedStore.swift
@@ -12,6 +12,10 @@ public class CoreDataFeedStore: FeedStore {
 	private let container: NSPersistentContainer
 	private let context: NSManagedObjectContext
 	
+	enum CoreDataError: Error {
+		case missingEntityName
+	}
+	
 	public init(storeURL: URL, bundle: Bundle = .main) throws {
 		container = try NSPersistentContainer.load(
 			named: "\(CoreDataFeedStore.self)",
@@ -26,11 +30,59 @@ public class CoreDataFeedStore: FeedStore {
 	}
 	
 	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-		
+		context.perform { [context] in
+			let feedImages: [CoreDataFeedImage] = feed.map { localImage in
+				let feedImage = CoreDataFeedImage(context: context)
+				feedImage.id = localImage.id
+				feedImage.imageDescription = localImage.description
+				feedImage.location = localImage.location
+				feedImage.url = localImage.url
+				return feedImage
+			}
+			
+			let coreDataFeed = CoreDataFeed(context: context)
+			coreDataFeed.timestamp = timestamp
+			coreDataFeed.feedImages = NSOrderedSet(array: feedImages)
+			
+			do {
+				try context.save()
+				completion(nil)
+			} catch {
+				completion(error)
+			}
+		}
 	}
 	
 	public func retrieve(completion: @escaping RetrievalCompletion) {
-		completion(.empty)
+		context.perform { [context] in
+			guard let name = CoreDataFeed.entity().name else {
+				return completion(.failure(CoreDataError.missingEntityName))
+			}
+			
+			let request = NSFetchRequest<CoreDataFeed>(entityName: name)
+			request.fetchBatchSize = 0
+			request.returnsObjectsAsFaults = false
+			
+			do {
+				if let feed = try context.fetch(request).first {
+					let localFeed = feed.feedImages
+						.compactMap { $0 as? CoreDataFeedImage }
+						.map {
+							return LocalFeedImage(
+								id: $0.id,
+								description: $0.imageDescription,
+								location: $0.location,
+								url: $0.url
+							)
+						}
+					completion(.found(feed: localFeed, timestamp: feed.timestamp))
+				} else {
+					completion(.empty)
+				}
+			} catch {
+				completion(.failure(error))
+			}
+		}
 	}
 }
 
